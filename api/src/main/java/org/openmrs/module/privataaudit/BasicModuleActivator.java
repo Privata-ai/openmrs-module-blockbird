b3900fe08e242e49a553f000ad3cbc911cbcce5b
micklynch <mick.lynch@gmail.com>
2020-05-01 14:48:00 -0400

    Renamed all references to Blockbird to Privata. Also made changes for new Java SDK

--- api/src/main/java/org/openmrs/module/privataaudit/BasicModuleActivator.java
+++ api/src/main/java/org/openmrs/module/privataaudit/BasicModuleActivator.java
@@ -11,7 +11,7 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.bbaudit;
+package org.openmrs.module.privataaudit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
